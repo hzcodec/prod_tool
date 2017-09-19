a1b70cdd615426b7744742b83960af9d9167014f
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-09-19 11:20:03 +0200

    More implementations

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import common
 import prodtest
 
 
+def print_const():
+    app = wx.GetApp()
+
+
 class MainFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         wx.Frame.__init__(self, *args, **kwargs)
@@ -59,11 +63,29 @@ class MainFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.onUnLock, id=102)
         self.Bind(wx.EVT_MENU, self.onAbout, id=103)
 
-    def onOpen(selfself, event):
-        pass
+    def onOpen(self, event):
+        print_const()
+        openFileDialog = wx.FileDialog(self, "Open", "", "", "ACX/TCX config files (*.txt)|*.txt", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        openFileDialog.ShowModal()
+        openFileDialog.GetPath()
+        #print openFileDialog.GetPath()
+
+        with open(openFileDialog.GetPath()) as f:
+            lines = f.readlines()
+
+        # pass configuration parameters to downloader and prodtest
+        pub.sendMessage('configListener', message=lines, fname=openFileDialog.GetFilename())
+        self.tabDownLoader.print_parameters()
+        openFileDialog.Destroy()
 
     def onSave(self, event):
-        pass
+        saveFileDialog = wx.FileDialog(self, "Save As", "", "", "ACX/TCX config files (*.txt)|*.txt", wx.FD_SAVE | wx.FD_OVERWRITE_PROMPT)
+        saveFileDialog.ShowModal()
+        saveFileDialog.GetPath()
+
+        print self.tabProdTest.configParameters
+
+        saveFileDialog.Destroy()
 
     def onLock(self, event):
         self.tabProdTest.lock_text_controls()
@@ -74,7 +96,42 @@ class MainFrame(wx.Frame):
             self.Close(True)
 
     def onUnLock(self, event):
-        pass
+        dialog = wx.TextEntryDialog(self, message="Enter Password", caption="Password query", style=wx.OK|wx.CANCEL|wx.TE_PASSWORD)
+        dialog.SetValue("")
+        result = dialog.ShowModal()
+
+        # check password if OK button was pressed
+        if result == wx.ID_OK:
+            passwd = dialog.GetValue()
+
+            if (passwd == 'Woodpecker'):
+
+                self.tabProdTest.txtCtrl_cl_max.Enable()
+                self.tabProdTest.txtCtrl_cl_min.Enable()
+                self.tabProdTest.txtCtrl_sl_ki.Enable()
+                self.tabProdTest.txtCtrl_sl_max.Enable()
+                self.tabProdTest.txtCtrl_sl_min.Enable()
+                self.tabProdTest.txtCtrl_has_switch.Enable()
+                self.tabProdTest.txtCtrl_power_margin.Enable()
+                self.tabProdTest.txtCtrl_power_factor.Enable()
+                self.tabProdTest.txtCtrl_brightness_lo.Enable()
+                self.tabProdTest.txtCtrl_brake_temp_ok.Enable()
+                self.tabProdTest.txtCtrl_brake_temp_hi.Enable()
+                self.tabProdTest.txtCtrl_brake_max_id.Enable()
+                self.tabProdTest.txtCtrl_brake_pos_ratio.Enable()
+                self.tabProdTest.txtCtrl_trajec_acc.Enable()
+                self.tabProdTest.txtCtrl_trajec_ret.Enable()
+                self.tabProdTest.txtCtrl_dominant_throttle_on.Enable()
+                self.tabProdTest.txtCtrl_max_motor_temp.Enable()
+                self.tabProdTest.txtCtrl_num_motor_ch.Enable()
+                self.tabProdTest.txtCtrl_idle_timeout.Enable()
+                self.tabProdTest.txtCtrl_rope_stuck_on.Enable()
+                self.tabProdTest.txtCtrl_iq_alpha.Enable()
+                self.tabProdTest.txtCtrl_speed_alpha.Enable()
+                self.tabProdTest.txtCtrl_undershoot.Enable()
+                self.tabProdTest.txtCtrl_speed_lim.Enable()
+                self.tabProdTest.txtCtrl_delay_start.Enable()
+
 
     def onAbout(self, event):
         licence = """
