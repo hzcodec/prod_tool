c5ae0735fdff3384ea32c1686db5a16a57298948
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-09-26 12:13:21 +0200

    create_lic updated to auto genereate licfile

--- create_lic.py
+++ create_lic.py
@@ -4,13 +4,17 @@
 #
 # > python create_lic.py
 #
-# copy result in to licensfile.lic
+# licensefile.lic is updated automatically by the script.
 
 import base64
 
-endDate = '2017-09-26'
+endDate = '2017-09-21'
 data = base64.b64encode(endDate.encode())
 print(data)
 
 data2 = base64.b64decode(data.decode())
 print(data2)
+
+licFile = open("licensfile.lic", "w")
+licFile.write(data)
+licFile.close()
