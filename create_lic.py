717fca1888c8ec027e7c35df6a57ad939c24b2ac
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-09-26 11:36:05 +0200

    Added comment in create_lic

--- create_lic.py
+++ create_lic.py
@@ -1,3 +1,11 @@
+# How to create license file.
+#
+# Update endDate, row 11 below. Then run script.
+#
+# > python create_lic.py
+#
+# copy result in to licensfile.lic
+
 import base64
 
 endDate = '2017-09-26'
